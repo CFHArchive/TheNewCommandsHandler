acdf275e52d16aa442f7c907cc8200a6eca9120f
creatorfromhell <daniel.viddy@gmail.com>
2021-07-15 19:15:50 -0400

    Finish Bukkit Implementation.

---
+++ Bukkit/src/net/tnemc/commands/bukkit/provider/BukkitPlayerProvider.java
@@ -0,0 +1,49 @@
+package net.tnemc.commands.bukkit.provider;
+
+import net.tnemc.commands.core.provider.PlayerProvider;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
+
+public class BukkitPlayerProvider implements PlayerProvider {
+
+  final CommandSender sender;
+
+  public BukkitPlayerProvider(CommandSender sender) {
+    this.sender = sender;
+  }
+
+  @Override
+  public UUID getUUID() {
+    if(!isPlayer()) {
+      return UUID.randomUUID();
+    }
+    return ((Player)sender).getUniqueId();
+  }
+
+  @Override
+  public String getName() {
+    return sender.getName();
+  }
+
+  @Override
+  public String getDisplayName(boolean strip) {
+    return sender.getName();
+  }
+
+  @Override
+  public boolean isPlayer() {
+    return sender instanceof Player;
+  }
+
+  @Override
+  public void sendMessage(String message) {
+    sender.sendMessage(message);
+  }
+
+  @Override
+  public boolean hasPermission(String permission) {
+    return sender.hasPermission(permission);
+  }
+}
